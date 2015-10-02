40889472e53d0611369496e6405dfb3f66731736
Sam Salisbury <samsalisbury@gmail.com>
2015-10-02 11:12:48 +0100

    Add dev watch script

---
+++ watcher.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+s() { say -v Daniel "$@" & }
+fin() { s "Bye!"; exit 0; }
+build() {
+	result=$(./install-dev.bash)
+	s $(echo "$result" | tail -n1)
+	echo "$result"
+}
+trap fin SIGINT SIGTERM
+while true; do
+	fswatch -ro . | build
+done
+
