8b1d3ac4999280baed9ad2435446d6d02486807e
Sam Salisbury <samsalisbury@gmail.com>
2015-10-02 10:24:16 +0100

    No need for docker daemon to make Dockerfile

--- commands/dockerfile.go
+++ commands/dockerfile.go
@@ -1,12 +1,9 @@
 package commands
 
 import (
-	"fmt"
-
 	"github.com/opentable/sous/build"
 	"github.com/opentable/sous/tools/cli"
 	"github.com/opentable/sous/tools/file"
-	"github.com/opentable/sous/tools/path"
 )
 
 func DockerfileHelp() string {
@@ -19,22 +16,12 @@ func Dockerfile(packs []*build.Pack, args []string) {
 		target = args[0]
 	}
 	feature, context, appInfo := AssembleFeatureContext(target, packs)
-	BuildDockerfileIfNecessary(feature, context, appInfo)
+	BuildDockerfile(feature, context, appInfo)
 	fp := context.FilePath("Dockerfile")
 	df, ok := file.ReadString(fp)
 	if !ok {
 		cli.Fatalf("Unable to read %s", fp)
 	}
 	cli.Outf(df)
-	cli.Success()
-}
-
-func BuildPathHelp() string {
-	return `sous build-path prints the working directory for this project's build state`
-}
-
-func BuildPath(packs []*build.Pack, args []string) {
-	_, context, _ := AssembleFeatureContext("build", packs)
-	fmt.Println(path.Resolve(path.BaseDir(context.BaseDir())))
 	cli.Success()
 }
