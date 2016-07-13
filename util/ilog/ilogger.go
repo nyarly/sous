90f92f22211f5c372348e5b02e51302330e10b38
Sam Salisbury <samsalisbury@gmail.com>
2016-07-13 18:00:38 +0100

    ilog: Add note about call depth to interface

--- util/ilog/ilogger.go
+++ util/ilog/ilogger.go
@@ -6,9 +6,21 @@ type (
 	ILogger interface {
 		// SetLogFunc sets the log function for standard log messages from this
 		// ILogger instance. The Watcher guarantees that this will never be nil.
+		//
+		// The file and line location of the reported debug call will be the
+		// file and line of the direct caller of the provided function.
+		// Therefore, in order to get meaningful file and line data, the
+		// implementor of this interface must ensure they use this function
+		// directly, with no wrappers, in their code.
 		SetLogFunc(func(...interface{}))
 		// SetDebugFunc is similar to SetLogFunc except it controls debug-level
 		// messages. SetDebugFunc will never be passed nil by Watcher.
+		//
+		// The file and line location of the reported debug call will be the
+		// file and line of the direct caller of the provided function.
+		// Therefore, in order to get meaningful file and line data, the
+		// implementor of this interface must ensure they use this function
+		// directly, with no wrappers, in their code.
 		SetDebugFunc(func(...interface{}))
 	}
 	// Level is the log level.
